f11041b184dfd3707201d8c0b75595940c2281a6
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-03-15 09:21:18 -0400

    hiya

---
+++ Cards/src/Card.java
@@ -0,0 +1,40 @@
+
+public class Card {
+ private String rank;
+ private String suit;
+ private int pointValue;
+ 
+ 
+ public Card(String rank, String suit, int pointValue)
+ {
+	 this.rank=rank;
+	 this.suit=suit;
+	 this.pointValue= pointValue;
+ }
+public String getRank() {
+	return rank;
+}
+public String getSuit() {
+	return suit;
+}
+public int getPointValue() {
+	return pointValue;
+}
+
+public boolean equals(Card otherCard)
+{
+	if(otherCard.pointValue==this.pointValue)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+public String toString()
+{
+	return(rank + "of" + suit + "with value" + pointValue);
+}
+
+}
+
